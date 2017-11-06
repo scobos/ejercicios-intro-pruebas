eb7bb91a11e17df9e0e07f0f2d6dd221592dd93a
scobos <sergiocobos21@gmail.com>
2017-11-06 12:09:20 +0100

    Añadido FindLast

--- code/FindLast/FindLast.java
+++ code/FindLast/FindLast.java
@@ -4,18 +4,18 @@ public class FindLast
 {
   /**
     * Find last index of element
-    * 
+    *
     *  @param x array to search
     *  @param y value to look for
     *  @return last index of y in x; -1 if absent
     *  @throws NullPointerException if x is null
     */
    public static int findLast (int[] x, int y)
-   { 
+   {
       // As the example in the book points out, this loop should end at 0.
-      for (int i=x.length-1; i > 0; i--)
+      for (int i=x.length-1; i >= 0; i--)
       {
-         if (x[i] == y) 
+         if (x[i] == y)
          {
             return i;
          }
@@ -33,7 +33,7 @@ public class FindLast
          System.out.println ("Usage: java FindLast v1 [v2] [v3] ... ");
          return;
       }
-   
+
       for (int i = 0; i< argv.length; i++)
       {
          try
@@ -48,10 +48,10 @@ public class FindLast
       }
       System.out.println ("Enter an integer you want to find: ");
       integer = getN();
-   
+
       System.out.println ("The index of the last element equal to " + integer + " is: " + findLast (inArr, integer));
    }
-   
+
    // ====================================
    // Read (or choose) an integer
    private static int getN ()
@@ -59,7 +59,7 @@ public class FindLast
       int inputInt = 1;
       BufferedReader in = new BufferedReader (new InputStreamReader (System.in));
       String inStr;
-   
+
       try
       {
          inStr    = in.readLine ();
@@ -73,7 +73,7 @@ public class FindLast
       {
          System.out.println ("Entry must be a number, choosing 1.");
       }
-   
+
       return (inputInt);
    }  // end getN
 }
